068882e28e4d275305cd61b865e80d603a0caaee
LuttaKing <denilsonwashuma@gmail.com>
2024-08-26 14:45:17 +0300

    Initial Commit

---
+++ TasksToDoMe/TasksToDoMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksToDoMeApp.swift
+//  TasksToDoMe
+//
+//  Created by Denilson Washuma on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TasksToDoMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
